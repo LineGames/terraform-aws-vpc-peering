24484d971f0b60ec454b9c4f5448c4049c9036c1
coille02 <kb.coille@gmail.com>
2022-12-26 02:40:44 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,5 +5,61 @@ terraform {
       version               = "~> 4"
       configuration_aliases = [aws.this, aws.peer]
     }
+}
+  
+provider "aws" {
+  alias      = "jp"
+  region     = "ap-northeast-1"
+
+  tags {
+      tags = {
+      Create-by = "Terraform"
+      }
+    }
   }
+
+provider "aws" {
+  alias      = "kr"
+  region     = "ap-northeast-2"
+  
+  tags {
+      tags = {
+      Create-by = "Terraform"
+      }
+    }
+  }
+
+provider "aws" {
+  alias      = "sg"
+  region     = "ap-southeast-1"
+  
+  tags {
+      tags = {
+      Create-by = "Terraform"
+      }
+    }
+  }
+
+provider "aws" {
+  alias      = "de"
+  region     = "ue-central-1"
+  
+  tags {
+      tags = {
+      Create-by = "Terraform"
+      }
+    }
+  }
+
+provider "aws" {
+  alias      = "us"
+  region     = "us-east-1"
+  
+  tags {
+      tags = {
+      Create-by = "Terraform"
+      }
+    }
+  }
+EOF
 }
