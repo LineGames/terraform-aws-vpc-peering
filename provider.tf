978c9e00d16b771d23ce27341fce773079a776bd
coille02 <kb.coille@gmail.com>
2022-12-26 02:55:33 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,63 +1,32 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 4"
-      configuration_aliases = [aws.this, aws.peer]
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 provider "aws" {
   alias      = "jp"
   region     = "ap-northeast-1"
-
-  tags {
-      tags = {
-      Create-by = "Terraform"
-      }
-    }
   }
 
 provider "aws" {
   alias      = "kr"
   region     = "ap-northeast-2"
-  
-  tags {
-      tags = {
-      Create-by = "Terraform"
-      }
-    }
   }
 
 provider "aws" {
   alias      = "sg"
   region     = "ap-southeast-1"
-  
-  tags {
-      tags = {
-      Create-by = "Terraform"
-      }
-    }
   }
 
 provider "aws" {
   alias      = "de"
   region     = "ue-central-1"
-  
-  tags {
-      tags = {
-      Create-by = "Terraform"
-      }
-    }
   }
 
 provider "aws" {
   alias      = "us"
   region     = "us-east-1"
-  
-  tags {
-      tags = {
-      Create-by = "Terraform"
-      }
-    }
   }
